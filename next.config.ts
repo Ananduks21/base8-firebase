22a3e45129cfe9243e8127bf422ba08e2a7433ea
Anandu K S <ananduks21@gmail.com>
2025-05-24 16:45:14 +0000

    Error: Invalid src prop (https://solumera.shop/cdn/shop/files/solumera_scrubber.jpg?v=1743702769&width=823) on `next/image`, hostname "solumera.shop" is not configured under images in your `next.config.js` See more info: https

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'solumera.shop',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
